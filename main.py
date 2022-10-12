d8646fc68095bf233fe7dacc95da86a477843e87
BuildTools <unconfigured@null.spigotmc.org>
2022-10-12 11:07:08 +0200

    now loading every image into list images and cropping them to 0-100*0-100

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ import pydicom
 import matplotlib.pyplot as plt
 import os
 
-# creating a path-list for every file in the directory /012_fmre_40Hz_SS_11sl_TR1200
-path = '012_fmre_40Hz_SS_11sl_TR1200'
-file_list = sorted([f.path for f in os.scandir(path)])
+def main(path: str):
+    # creating a path-list for every file in the directory path
+    file_list = sorted([f.path for f in os.scandir(path)])
+
+    #creating a list  with every first 100 times 100 pixel out of every file
+    images = [[i[:100] for i in pydicom.dcmread(file_list[j]).pixel_array[:100]] for j in range(len(file_list))]
+
+    image = images[0]
+    plt.imshow(image)
+    plt.colorbar()
+    plt.show()
 
 
-i = 0
-ds = pydicom.dcmread(file_list[i])
-
-plt.imshow(ds.pixel_array)
-plt.show()
-
-# matplotlib.pyplot ansehen
+main(path='012_fmre_40Hz_SS_11sl_TR1200')
