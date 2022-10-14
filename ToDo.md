469cf8b070c05fc41cbe2b00a4d103ff29796212
BuildTools <unconfigured@null.spigotmc.org>
2022-10-14 16:08:51 +0200

    now computing and displaying data from every slice

--- ToDo.md
+++ ToDo.md
@@ -2,4 +2,6 @@
 - [x] Standartabweichung
 - [x] SNR berechnen und plotten
 - [x] Eigene Maske für jedes Bild (Derzeit: Maske des ersten Bilds (Image[0]) für alle)
-- [ ] Alle slices auf einmal auswerten (Derzeit: immer nur ein slice)
+- [x] Alle slices auf einmal auswerten (Derzeit: immer nur ein slice)
+- [x] alle slices gemeinsam plotten
+- [ ] sieht schön aus
--- main.py
+++ main.py
@@ -1,70 +1,86 @@
+from time import time
 from pydicom import dcmread
-import matplotlib.pyplot as plt
 from os import scandir
 import numpy as np
 from scipy.ndimage import binary_fill_holes, binary_erosion, binary_dilation
 from math import sqrt, log10
+import matplotlib.pyplot as plt
+start_time = time()
 
-def main(path: str, coord_slice: int):
-    # calculating the coordinates for the slice
-    #xslice = 100 * (coord_slice % 4)
-    #yslice = 100 * (coord_slice // 4 + 1)
+def main(path: str, image_count: int, slices: int):
 
     # creating a path-list for every file in the directory path
     file_list = sorted([f.path for f in scandir(path)])
-    image_count = len(file_list)
 
-    # creating a list  with every first 100 times 100 pixel out of every file
-    images = [np.array([i[100:200] for i in dcmread(file_list[j]).pixel_array[100:200]]) for j in range(image_count)]
+    # creating a list with a list for every slice with every first 100 times 100 pixel out of every file
+    images = np.array([np.array([np.array([i[100 * (k % 4):100 * (k % 4) + 100] for i in dcmread(file_list[j]).pixel_array[100 * (k // 4):100 * (k // 4) + 100]]) for j in range(image_count)]) for k in range(slices)])
 
     # creating binary mask based for every image
-    masks_signal = [i > 100 for i in images]
-    masks_signal = [binary_fill_holes(i).astype(int) for i in masks_signal]
-    masks_signal = [binary_erosion(i, iterations=3).astype(int) for i in masks_signal]
-    masks_signal = [binary_dilation(i, iterations=3).astype(int) for i in masks_signal]
+    masks_signal = [[i > 100 for i in images[j]] for j in range(slices)] # creating mask
+    masks_signal = [[binary_fill_holes(i).astype(int) for i in j] for j in masks_signal] # filling holes
+    masks_signal = [[binary_dilation(binary_erosion(i, iterations=3), iterations=3).astype(int) for i in j] for j in masks_signal] # removing halo
+
     # creating a mask for noise
     mask_noise = np.zeros((100, 100))
     mask_noise[0:20, 0:20] = 1
 
-    # just like images, but with the mask_signal applied (and np.nan instead of 0)
-    signal_masked_images = [np.multiply(j, masks_signal[i]).astype('float') for i, j in enumerate(images)]
-    for i, j in enumerate(signal_masked_images):
-        signal_masked_images[i][signal_masked_images[i] == 0] = np.nan
-    # same for mask_noise
-    noise_masked_images = [np.multiply(i, mask_noise).astype('float') for i in images]
-    for i, j in enumerate(noise_masked_images):
-        noise_masked_images[i][noise_masked_images[i] == 0] = np.nan
+    # new
+    signal_masked_images = np.array([None for _ in range(slices)])
+    for k in range(slices):
+        this_slice = np.array([np.multiply(j, masks_signal[k][i]).astype('float') for i, j in enumerate(images[k])])
+        for i, j in enumerate(this_slice):
+            this_slice[i][this_slice[i] == 0] = np.nan
+        signal_masked_images[k] = this_slice
 
-    # the mean value from every masked image
-    signal_mean_values = [np.nanmean(signal_masked_images[i]) for i in range(image_count)]
-    signal_mean = np.nanmean(signal_mean_values)
-    # same for noise
-    noise_mean_values = [np.nanmean(noise_masked_images[i]) for i in range(image_count)]
+    # new
+    noise_masked_images = np.array([None for _ in range(slices)])
+    for k in range(slices):
+        this_slice = np.array([np.multiply(j, mask_noise).astype('float') for i, j in enumerate(images[k])])
+        for i, j in enumerate(this_slice):
+            this_slice[i][this_slice[i] == 0] = np.nan
+        noise_masked_images[k] = this_slice
+
+    signal_mean_values = [[np.nanmean(signal_masked_images[k][i]) for i in range(image_count)] for k in range(slices)]
+
+    signal_mean = [np.nanmean(i) for i in signal_mean_values]
+
+    noise_mean_values = [[np.nanmean(noise_masked_images[k][i]) for i in range(image_count)] for k in range(slices)]
 
     # calculating the standard deviation for signal
-    signal_standartdeviation = sqrt(sum([(i-signal_mean) ** 2 for i in signal_mean_values]) / image_count)
+    signal_standartdeviation = [standard_deviation(signal_mean_values[i], signal_mean[i]) for i in range(slices)]
 
     # calculating SNR (SignalNoiseRatio)
-    snr = [20 * log10(signal_mean_values[i] / noise_mean_values[i]) for i in range(image_count)]
+    snr = [[20 * log10(signal_mean_values[k][i] / noise_mean_values[k][i]) for i in range(image_count)] for k in range(slices)]
 
     # plotting the data
-    range_image_count = range(image_count + 1)[1:]
-    fig, (ax1, ax2) = plt.subplots(2, 1, sharex="col")
-    ax1.plot(range_image_count, signal_mean_values, "b.-") # graph
-    ax1.plot(range_image_count, [signal_mean for _ in range(image_count)], "m-") # mean
-    ax1.plot(range_image_count, [signal_mean + signal_standartdeviation for _ in range(image_count)], "y-") # standart deviation upper
-    ax1.plot(range_image_count, [signal_mean - signal_standartdeviation for _ in range(image_count)], "y-") # standart deviation lower
-    ax2.plot(range_image_count, snr, "r.-")
+    range_image_count1 = range(image_count + 1)[1:] # array for the length of the y-axis
+    fig, ax = plt.subplots(slices, 2)#, sharex="none")
+    fig.subplots_adjust(hspace=0)
+    for k in range(slices):
+        ax[k, 0].plot(range_image_count1, signal_mean_values[k], "b.-")
+    ax[0, 0].plot(range_image_count1, [signal_mean[0] for _ in range(image_count)], "m-") # mean
+    ax[0, 0].plot(range_image_count1, [signal_mean[0] + signal_standartdeviation[0] for _ in range(image_count)], "y-") # standart deviation upper
+    ax[0, 0].plot(range_image_count1, [signal_mean[0] - signal_standartdeviation[0] for _ in range(image_count)], "y-") # standart deviation lower
+    for k in range(slices):
+        ax[k, 1].plot(range_image_count1, snr[k], "r.-")
 
-    ax1.set_xlabel("scan")
-    ax2.set_xlabel("scan")
-
-    ax1.set_ylabel("mean value")
-    ax2.set_ylabel("SNR")
-
-    #ax.grid(axis="y")
+    ax[0, 0].set_xlabel("scan")
+    #ax[1].set_xlabel("scan")
+    for k in range(slices):
+        ax[k, 0].set_ylabel(f"slice {k + 1}")
+    for k in range(slices):
+        ax[k, 0].grid(axis="y")
     fig.suptitle('Plot')
-    #plt.axis([0, image_count, min(signal_mean_values) - min(signal_mean_values) % 20, max(signal_mean_values) + (20 - max(signal_mean_values) % 20)])
+    #for k in range(slices):
+    #    for i in range(2):
+    #        ax[k, i].axis([0, image_count, min(signal_mean_values[k]) - min(signal_mean_values[k]) % 20, max(signal_mean_values) + (20 - max(signal_mean_values[k]) % 20)])
+    figmanager = plt.get_current_fig_manager()
+    figmanager.full_screen_toggle()
     plt.show()
 
-main('012_fmre_40Hz_SS_11sl_TR1200', 3)
+def standard_deviation(values: list, mean=None, count=None):
+    return sqrt(sum([(i-(mean if not mean else np.mean(values))) ** 2 for i in values]) / (count if count != None else len(values)))
+
+main('012_fmre_40Hz_SS_11sl_TR1200', 155, 11)
+
+print("Runtime: ", time() - start_time)
